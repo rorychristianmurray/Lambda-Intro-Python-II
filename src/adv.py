5a49612706acf3a86decc5ed3053021643def71b
Rory Murray <rory.christian.murray@gmail.com>
2019-10-30 22:44:19 -0400

    Create repl input, create Room class

--- src/adv.py
+++ src/adv.py
@@ -1,7 +1,8 @@
+import argparse
 from room import Room
 
 # Declare all the rooms
-
+# uncomment block
 room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
@@ -21,21 +22,59 @@ chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
 
+print("narrow:\n ", room['narrow'])
+
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+# uncomment block
+# room['outside'].n_to = room['foyer']
+# room['foyer'].s_to = room['outside']
+# room['foyer'].n_to = room['overlook']
+# room['foyer'].e_to = room['narrow']
+# room['overlook'].s_to = room['foyer']
+# room['narrow'].w_to = room['foyer']
+# room['narrow'].n_to = room['treasure']
+# room['treasure'].s_to = room['narrow']
 
 #
 # Main
 #
+
+# List
+# Create REPL parser which takes one arg [DONE]
+# that arg has 4 valid commands, n, s, e, and w [DONE]
+# if player moves to occupied room throw an error
+
+
+# define program description
+text = 'This is a text based game'
+
+# initiate the parser
+parser = argparse.ArgumentParser(description=text)
+
+parser.add_argument("-V", "--version",
+                    help="Shows program version", action="store_true")
+parser.add_argument("--width", "-w", help="set output width")
+
+parser.add_argument("direction", help="set direction of character move. Valid moves are n, s, e, and w", choices=[
+                    'n', 's', 'e', 'w', 'q'])
+
+# parse args
+args = parser.parse_args()
+print("args : ", args)
+
+# check for --version or -V
+if args.version:
+    print("this is myprogram version 0.1")
+
+# check for --width
+if args.width:
+    print("set output width to %s" % args.width)
+
+d = args.direction
+print("d: ", d)
+
 
 # Make a new player object that is currently in the 'outside' room.
 
--- src/room.py
+++ src/room.py
@@ -1,2 +1,18 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+# Create room class
+# give it attributes from adv.py
+
+
+class Room:
+    def __init__(self, name, desc):  # constructor
+        self.name = name
+        self.desc = desc
+
+    def __str__(self):
+        return f"Name: {self.name}\n Desc: {self.desc}\n"
+
+
+# r = Room("Duskery", "A sad place, really")
+# print("r : ", r)
